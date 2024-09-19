b913d1c09006167e6023aa761c51ece722d5e6c3
IdiotStinker <saltylightning564@gmail.com>
2024-09-19 21:20:42 +0000

    encrypt (in progress)

--- EPICCOOL.py
+++ EPICCOOL.py
@@ -1,1 +1,19 @@
-toierhg;selrkghaef;lkghef;oigbf;gkjeg;klern
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet.index("a")
+cool = "IDK MAN WHAT DO U WANT"
+
+def encrypt(x, a, c):
+    return (x * a + c) % len(alphabet)
+
+def encrypt_text(txt):
+    for char in txt:
+        index = encrypt(alphabet.index(char), 5, 2)
+        newChar = alphabet[index]
+        array.append(newChar)
+
+    return "".join(array)
+
+def decrypt(x, a, c):
+    return 
+
+print(encrypt_text(cool))
