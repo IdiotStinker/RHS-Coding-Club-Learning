a04003aace620383a883e62f00b950b90d048bb9
IdiotStinker <saltylightning564@gmail.com>
2024-10-09 20:25:24 -0700

    Added data for next arrow problem with numbers

--- ArrowMazeThingy.py
+++ ArrowMazeThingy.py
@@ -1,19 +1,41 @@
-import sys
-sys.setrecursionlimit(10000000)
-
-data = [
+dataArrow = [
     [[0, 0], [0, 1], [0, -1], [1, 0]],
-    [[1, 0], [-1, -1], [-1, 0], [-1, 1]],
-    [[1, 0], [1, 1], [-1, -1], [0, -1]],
+    [[1, 0], [-1, -1], [0, -1], [1, -1]],
+    [[0, 1], [1, 1], [-1, -1], [0, -1]],
     [[-1, 1], [0, -1], [-1, 1], [-1, 0]]
 ]
 
-grid = [
+data = [
     [16, -1, 1, -1],
     [-1, -1, -1, -1],
     [-1, -1, -1, -1],
     [-1, -1, -1, -1]
 ]
+
+dataArrow = [
+    [[0, 1], [0, 1], [0, 1], [1, 1], [1, 1], [1, -1], [0, -1]],
+    [[1, 0], [0, 1], [1, 0], [1, -1], [0, -1], [1, 0], [1, 0]],
+    [[0, 1], [-1, -1], [-1, 0], [1, -1], [-1, 0], [0, -1], [0, -1]],
+    [[0, 1], [0, 1], [1, 0], [1, -1], [0, -1], [1, 0], [1, -1]],
+    [[-1, 1], [0, 1], [-1, 0], [1, 0], [1, 0], [1, 1], [0, -1]],
+    [[-1, 0], [0, -1], [0, 1], [0, 1], [-1, 1], [-1, -1], [-1, -1]],
+    [[-1, 1], [0, 1], [0, 1], [0, 1], [0, -1], [-1, -1], [0, 0]],
+]
+
+data = [
+    [1, -1, 2, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1],
+    [-1, 6, 33, -1, -1, -1, -1],
+    [-1, -1, 32, -1, -1, -1, 28],
+    [11, -1, -1, -1, -1, -1, -1],
+    [17, -1, 47, -1, 45, -1, 49]
+]
+
+grid = []
+
+for row in data:
+    grid.append([char for char in row])
 
 N = len(grid)
 
@@ -21,45 +43,48 @@ def backTrack(row, col, value, start = False):
     
     if start:
         for r, fullRow in enumerate(grid):
-            for c, value in enumerate(fullRow):
-                if value == 1:
+            for c, val in enumerate(fullRow):
+                if val == 1:
                     row = r
                     col = c
 
     if row == N: 
         print("huh")
         return True
-    if col == N: 
+    if col == N:
         print("heh")
         return backTrack(row + 1, col, value)
 
-    if not (grid[row][col] == -1 or start): 
-        print("hm")
+    if not (grid[row][col] == -1 or start):
         return False
 
     spots = findSpotsInDirection(row, col)
 
-    for i in range(spots):
-
-        dr = data[row][col][0]
-        dc = data[row][col][1]
-
+    for spotIndex in range(spots):
+        dr = dataArrow[row][col][0]
+        dc = dataArrow[row][col][1]
         grid[row][col] = value
-        if backTrack(row + dr * (i + 1), col + dc * (i + 1), value + 1): return True
+        if backTrack(row + dr * (spotIndex + 1), col + dc * (spotIndex + 1), value + 1): return True
 
+    if value == N^2:
+        return True
+    print(value)
     grid[row][col] = -1
     return False
 
 def findSpotsInDirection(row, col):
-    dr = data[row][col][0]
-    dc = data[row][col][1]
+    dr = dataArrow[row][col][0]
+    dc = dataArrow[row][col][1]
     spots = 0
+    if dr == 0 and dc == 0:
+        print("error")
+        return 0
+    
     while 0 <= row + dr < N and 0 <= col + dc < N:
         row += dr
         col += dc
         spots += 1
 
-    print(spots)
     return spots
 
 def print_sol():
@@ -74,11 +99,7 @@ for r in range(N):
         else:
             findSpotsInDirection(r, c)
 
-
-print("why")
-
-
-#if not backTrack(0, 2, 1, True):
-#    print ("very sad")
-#print_sol()
+if not backTrack(0, 2, 1, True):
+    print ("very sad")
+print_sol()
 
