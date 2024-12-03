e8ee7671b2f34fa4f64e17165d0c9cc2b8b6d014
IdiotStinker <saltylightning564@gmail.com>
2024-12-03 23:37:18 +0000

    commit

--- AdventCode2023Day8.py
+++ AdventCode2023Day8.py
@@ -1,36 +1,44 @@
 pattern = "LRLRLRLLRRLRRLRRRLRRLRLLRRRLRRRLRRLLLLRRRLRLLRRLRRLRRLLLRRRLRRRLRRLRLRRLRLRLRLLRRRLRRRLLRRRLRRRLRRRLRLLLRRLRLRRRLRLRRRLLRRRLRLLRLRRRLRLRRRLRRLLRLRLRRLRLRLRRLRLRLRRRLRRLRLLRRLRRRLRRRLRRLRRRLRRLRLRRRLLRRRLLRRLRLRRRLRRRLLRRRLRLRRLRLRLRRLRLLRRLRLRLRRLRRRLRRRLRLRRLRRLLLRRRLLRLRRRLLRRRR"
-with open("2023Day8.txt", "r") as file:
+with open("Year2023Day8.txt", "r") as file:
     a = file.read()
 
-print(a)
+
 
 b = a.split("\n")
 
-print(b)
-
-diction = {
-    
-}
+diction = {}
 
 current = "AAA"
+currentList = []
 i = 0
 times = 0
 aCount = 0
+aDict = []
+correctList = []
 for stuff in b:
     diction[stuff[0:3]] = stuff[7:15].split(", ")
     if stuff[2] == "A":
         aCount+=1
-
-print(diction)
-
-while current != "ZZZ":
-    if pattern[i] == "L":
-        current = diction[current][0]
-    else:
-        current = diction[current][1]
-    i+=1
+        currentList.append(stuff[0:3])
+        correctList.append(False)
+maybe = False
+while not maybe:
+    for j, dictCurrent in enumerate(currentList):
+        if pattern[i] == "L":
+            currentList[j] = diction[dictCurrent][0]
+        elif pattern[i] == "R":
+            currentList[j] = diction[dictCurrent][1]
+    
+    i+=1 
     if len(pattern) == i:
-        i = 0
+        i = 0 
+    
     times+=1
+    
+    for index, cool in enumerate(currentList):
+        if cool[2] == "Z":
+            correctList[index] = True
+        else:
+            correctList[index] = False
 
 print(times)
