10e557af5e5e7eb44f1d245231a0022245e06a0b
IdiotStinker <saltylightning564@gmail.com>
2024-12-03 20:59:25 -0800

    save

--- AdventCode2023Day8.py
+++ AdventCode2023Day8.py
@@ -12,22 +12,26 @@ current = "AAA"
 currentList = []
 i = 0
 times = 0
-aCount = 0
 aDict = []
 correctList = []
+endLoop = False
 for stuff in b:
     diction[stuff[0:3]] = stuff[7:15].split(", ")
     if stuff[2] == "A":
-        aCount+=1
+        print(stuff[0:3])
         currentList.append(stuff[0:3])
         correctList.append(False)
-maybe = False
-while not maybe:
-    for j, dictCurrent in enumerate(currentList):
+
+
+while not endLoop:
+
+    for j, item in enumerate(currentList):
         if pattern[i] == "L":
-            currentList[j] = diction[dictCurrent][0]
+            currentList[j] = diction[item][0]
         elif pattern[i] == "R":
-            currentList[j] = diction[dictCurrent][1]
+            currentList[j] = diction[item][1]
+        else:
+            print("error")
     
     i+=1 
     if len(pattern) == i:
@@ -35,10 +39,20 @@ while not maybe:
     
     times+=1
     
-    for index, cool in enumerate(currentList):
+    for k, cool in enumerate(currentList):
         if cool[2] == "Z":
-            correctList[index] = True
+            correctList[k] = True
         else:
-            correctList[index] = False
+            correctList[k] = False
+
+    for bool in correctList:
+        if bool != True:
+            endLoop = False
+            break
+        else:
+            endLoop = True
+
+    
+    
 
 print(times)
